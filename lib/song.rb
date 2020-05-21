89ee0562b7139aad75aef2408a9e71f47b6d6b87
Jake G <jake098877@gmail.com>
2020-05-21 15:57:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,43 @@
 require 'pry'
 class Song
-attr_accessor :name, :artist
-@@all = []
+  attr_accessor :name, :artist
+  @@all = []
 
-def initialize(name, artist=nil, genre=nil)
-    @name = name
-    @@all
-end
+  def initialize(name, artist=nil, genre=nil)
+      @name = name
+      @@all
+  end
 
-def save
-    @@all << self
-end
+  def save
+      @@all << self
+  end
 
-def self.all
-    @@all
-end
+  def self.all
+      @@all
+  end
 
-def self.destroy_all
-    @@all.clear
-end
+  def self.destroy_all
+      @@all.clear
+  end
 
-def self.create(name)
-    self.new(name)
-    self.save
-end
+  def self.create(name)
+      self.new(name)
+      self.save
+  end
 
-def artist=(artist)
-    @artist = artist
-    Artist.song = self
-end
+  def artist=(artist)
+      @artist = artist
+      Artist.song = self
+  end
 
-def find_by_name(name)
-    self.all.detect {|song| song.name == name}
-end
-end
+  def find_by_name(name)
+      self.all.detect {|song| song.name == name}
+  end
+  end
 
-def find_or_create_by_name(name)
-    if !self.all.detect {|song| song.name == name}
-        Song.new(name)
-end
-end
+  def find_or_create_by_name(name)
+      if !self.all.detect {|song| song.name == name}
+          Song.new(name)
+  end
+  end
 end
